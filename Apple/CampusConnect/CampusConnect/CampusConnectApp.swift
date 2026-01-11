f52e4f7ca411d6783e268fc5eebcb316177c89e1
Edgars Yarmolatiy <e.yarmolatiy@gmail.com>
2026-01-11 17:28:05 -0600

    refactor: convert Apple/CampusConnect from submodule to regular directory
    Unified repository structure to simplify development workflow. All platforms (web, iOS) now share the same Git history and can be committed together.

---
+++ Apple/CampusConnect/CampusConnect/CampusConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusConnectApp.swift
+//  CampusConnect
+//
+//  Created by Edgars Yarmolatiy on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
